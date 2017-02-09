aac0b6b474b65aee865dd7e34958b8ccae9405ab
granter1 <sean.grant.1@ucdconnect.ie>
2017-02-09 12:20:35 +0000

    Commit 2
    Added player tokens to Board. Players can move all around the board and
    each move is shown.

--- DeadWood.java
+++ DeadWood.java
@@ -1,4 +1,4 @@
-/* DeadWood Monopoly game - Se√°n Grant 15347791
+/* DeadWood Monopoly game - Se·n Grant 15347791
                             Aonghus Condren 15348051
                             Conall Doherty 15468482 */
 
@@ -8,18 +8,8 @@
 
   public class DeadWood
   {
-    public DeadWood()
-    {
-      JFrame frame = new JFrame();
-      frame.add(new Board());
-      frame.setSize(540,560);
-      frame.setTitle("Game Board");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setVisible(true);
-    }
-
     public static void main(String[] args)
     {
-      DeadWood board = new DeadWood();
-    }
+      Board.LoadBoard();
+    } 
   }
