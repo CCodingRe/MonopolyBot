eb2a93bbff85658f0f68d616030695bc039ee0e9
dConall <conall.doherty.1@ucdconnect.ie>
2017-02-25 22:12:06 +0000

    Fixed wrong type issue info and moved info into move

--- deadwood/TurnControl.java
+++ deadwood/TurnControl.java
@@ -36,33 +36,31 @@ public class TurnControl{
                     cmdCheck(element);
                   }
               }
+              Info_Panel.UserInput(SetUp.getLocationsList().get(element.getLocation()).getName());
+              if(SetUp.getLocationsList().get(element.getLocation()) instanceof Propertys){
+                Info_Panel.UserInput("Cost: " + Integer.toString(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getValue()));
+                if(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getOwner() != null){
+                  Info_Panel.UserInput(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getOwnerName());
+                } else {
+                  Info_Panel.UserInput("No Owner");
+                }
+              }
               break;
 
-        case "info" :
-            Info_Panel.UserInput(SetUp.getLocationsList().get(element.getLocation()).getName());
-            Info_Panel.UserInput(Integer.toString(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getValue()));
-            if(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getOwner() != null){
-              Info_Panel.UserInput(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getOwnerName());
-            } else {
-              Info_Panel.UserInput("No Owner");
-            }
-            break;
-
-        /*case "buy" :
+        case "buy" :
             if(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getOwner() == null){
               element.buy(((Propertys) SetUp.getLocationsList().get(element.getLocation())).getValue(); //takes money away from players balance
               ((Propertys) SetUp.getLocationsList().get(element.getLocation())).setOwner(element); //sets owner of property
             } else {
               Info_Panel.UserInput("Property already bought");
             }
-            break;*/
+            break;
         case "balance" :
             Info_Panel.UserInput(Integer.toString(element.getBalance()));
 
         case "help" :
             Info_Panel.UserInput("type move to move player");
             Info_Panel.UserInput("type buy to buy property");
-            Info_Panel.UserInput("type info for details on current property");
             Info_Panel.UserInput("type balance to get bank balance");
             Info_Panel.UserInput("type done when you are finished your turn");
             Info_Panel.UserInput("type quit to end game");
