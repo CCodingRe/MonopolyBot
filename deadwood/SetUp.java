c0c9a9c10f5cbd202484e6ca5129dfae8ade18c6
aongeese <aonghus.condren@ucdconnect.ie>
2017-04-09 16:22:46 +0100

    fixed the error message for initial roll

--- deadwood/SetUp.java
+++ deadwood/SetUp.java
@@ -105,10 +105,10 @@ public class SetUp{
 
  							Info_Panel.UserInput(users.get(possDouble).getName() + "and " + users.get(otherPossDouble).getName() +" rolled the same, they must roll again!");
 
-							int aTotal = users.get(otherPossDouble).getFirstRoll() + TurnControl.roll(users.get(otherPossDouble));
+							int aTotal = users.get(otherPossDouble).getFirstRoll() + TurnControl.initialRoll(users.get(otherPossDouble));
 							users.get(otherPossDouble).setFirstRoll(aTotal);
 
-							int otherTotal =  users.get(possDouble).getFirstRoll() + TurnControl.roll(users.get(possDouble));
+							int otherTotal =  users.get(possDouble).getFirstRoll() + TurnControl.initialRoll(users.get(possDouble));
 							users.get(possDouble).setFirstRoll(otherTotal);
 					}
 				}
