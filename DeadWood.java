19a7d9df4952ee2183885588b7ae60f79ad458d8
granter1 <sean.grant.1@ucdconnect.ie>
2017-04-23 19:05:22 +0100

    Bot now mortgages cheapest property and pays out of jail
    If the bot cannot afford to buy or has a balance below 0 it will
    mortgage it's cheapest property. Bot also pays out of jail because bug
    was occuring when thry rolled double out and then kept trying to roll
    after that even though their turn should be done

--- DeadWood.java
+++ DeadWood.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class DeadWood implements Bot {
 
@@ -8,6 +9,8 @@ public class DeadWood implements Bot {
 
 	private boolean rollDone = false;
 	private Property prop;
+	//private Square square;
+	private ArrayList<Property> ownedProperty;
 	BoardAPI boardBot; 
 	PlayerAPI playerBot; 
 	DiceAPI diceBot;
@@ -24,22 +27,33 @@ public class DeadWood implements Bot {
 	}
 
 	public String getCommand() {
-		if(diceBot.isDouble()) {
-			rollDone = false;
-		}
+		isReroll();
+		
 		//square = board.getSquare(player.getPosition());
 		if(boardBot.isProperty(playerBot.getPosition())) {
 			prop = boardBot.getProperty(playerBot.getPosition());
 		}
-
+		if(prop!=null) {
+			if(prop.getOwner()==null) {
+				if(playerBot.getBalance() < prop.getPrice()) {
+					return mortgageCheapestProperty();
+				}
+				return "buy";
+			}
+		}
+		
+		if(playerBot.isInJail()) {
+			rollDone = true;
+			return getDecision();
+		}
+		
 		if(!rollDone) {
 			rollDone = true;
 			return "roll";
 		}
-		if(prop!=null) {
-			if(prop.getOwner()==null) {
-				return "buy";
-			}
+		
+		if(playerBot.getBalance() < 0) {
+			return mortgageCheapestProperty();
 		}
 		
 		rollDone = false;
@@ -49,6 +63,24 @@ public class DeadWood implements Bot {
 	public String getDecision() {
 		// Add your code here
 		return "pay";
+	}
+	
+	private void isReroll() {
+		if(diceBot.isDouble()) {
+			rollDone = false;
+			if(playerBot.isInJail()) rollDone=true;
+		}
+	}
+	
+	private String mortgageCheapestProperty() {
+		ownedProperty = playerBot.getProperties();
+		Property cheapestProp = ownedProperty.get(0);
+		for(Property currProp : ownedProperty) {
+			if(cheapestProp.getPrice() > currProp.getPrice()) {
+				cheapestProp = currProp;
+			}
+		}
+		return "mortgage " + cheapestProp.getShortName();
 	}
 
 }
