b87551be57354a944abf89eaabb48d59daef1ab0
aongeese <aonghus.condren@ucdconnect.ie>
2017-04-24 17:02:15 +0100

    paying out of jail strat

--- DeadWood.java
+++ DeadWood.java
@@ -10,9 +10,9 @@ public class DeadWood implements Bot {
 	private boolean rollDone = false;
 	private Property prop;
 	//private Square square;
-	//private ArrayList<Property> ownedProperty;
-	BoardAPI boardBot; 
-	PlayerAPI playerBot; 
+	private ArrayList<Property> ownedProperty;
+	BoardAPI boardBot;
+	PlayerAPI playerBot;
 	DiceAPI diceBot;
 
 	DeadWood (BoardAPI board, PlayerAPI player, DiceAPI dice) {
@@ -28,13 +28,14 @@ public class DeadWood implements Bot {
 
 	public String getCommand() {
 		String command = "done";
-		
+
 		isReroll();
-		command = tryToBuyProperty();
 		if(command=="done") command = checkForJail();
-		if(command=="done") command = tryToRoll();
+		if(command=="done") command = tryToBuyProperty();
+		if(command=="done") command = tryToRedeem();
 		if(command=="done") command = checkForNegativeBal();
-		
+		if(command=="done") command = tryToRoll();
+
 		if(command=="done") rollDone = false;
 		return command;
 	}
@@ -43,37 +44,65 @@ public class DeadWood implements Bot {
 		// Add your code here
 		return "pay";
 	}
-	
-	
+
+
 	private void isReroll() {
 		if(diceBot.isDouble()) {
 			rollDone = false;
 			if(playerBot.isInJail()) rollDone=true;
 		}
 	}
-	
+
 	private String tryToBuyProperty() {
 		//square = board.getSquare(player.getPosition());
 		if(boardBot.isProperty(playerBot.getPosition())) {
 			prop = boardBot.getProperty(playerBot.getPosition());
 		}
 		if(prop!=null && prop.getOwner()==null) {
-				if(playerBot.getBalance() < prop.getPrice()) {
-					return mortgageCheapestProperty();
-				}
-				return "buy";
+			if(playerBot.getBalance() < prop.getPrice()) {
+				return mortgageCheapestProperty();
+			}
+			return "buy";
 		}
+		prop=null;
 		return "done";
 	}
-	
-	private String checkForJail() {
+
+	/*	private String checkForJail() {
 		if(playerBot.isInJail()) {
 			rollDone = true;
 			return getDecision();
 		}
 		return "done";
+	}*/
+
+	private String checkForJail() {
+		if(playerBot.isInJail()) {
+			rollDone = true;
+			int ownedProp = 0;
+
+			for(int checkProperties = 0; checkProperties<40; checkProperties++)
+			{
+				if(boardBot.isProperty(checkProperties))
+				{
+					if(boardBot.getProperty(checkProperties).getOwner() != null)
+					{
+						ownedProp +=1;
+					}
+				}
+			}
+
+			if(ownedProp>=24)
+			{
+				return "roll";
+			}else{
+				return getDecision();
+			}
+		}
+		return "done";
 	}
-	
+
+
 	private String tryToRoll() {
 		if(!rollDone) {
 			rollDone = true;
@@ -81,16 +110,16 @@ public class DeadWood implements Bot {
 		}
 		return "done";
 	}
-	
+
 	private String checkForNegativeBal() {
 		if(playerBot.getBalance() < 0) {
 			return mortgageCheapestProperty();
 		}
 		return "done";
 	}
-	
+
 	private String mortgageCheapestProperty() {
-		ArrayList<Property> ownedProperty = playerBot.getProperties();
+		ownedProperty = playerBot.getProperties();
 		if(ownedProperty != null) {
 			Property cheapestProp = null;
 			int i=0;
@@ -107,6 +136,36 @@ public class DeadWood implements Bot {
 					}
 				}
 				return "mortgage " + cheapestProp.getShortName();
+			}
+		}
+		return "done";
+	}
+
+	private String tryToRedeem() {
+		if(playerBot.getBalance() > 600) {
+			return redeemMostExpensiveProperty();
+		}
+		return "done";
+	}
+
+	private String redeemMostExpensiveProperty() {
+		ownedProperty = playerBot.getProperties();
+		if(ownedProperty != null) {
+			Property expensiveProp = null;
+			int i=0;
+			while(expensiveProp==null && i<ownedProperty.size()) {
+				if(ownedProperty.get(i).isMortgaged()) {
+					expensiveProp = ownedProperty.get(i);
+				}
+				i++;
+			}
+			if(expensiveProp!=null) {
+				for(Property currProp : ownedProperty) {
+					if(expensiveProp.getPrice() < currProp.getPrice() && currProp.isMortgaged()) {
+						expensiveProp = currProp;
+					}
+				}
+				return "redeem " + expensiveProp.getShortName();
 			}
 		}
 		return "done";
